00b89a86277cc190ec0265c38ada5f89bd70830e
shazhko <shazhko.artem@gmail.com>
2020-04-30 14:04:35 +0300

    add new workaround

--- EFCore.Bugs/Program.cs
+++ EFCore.Bugs/Program.cs
@@ -16,9 +16,10 @@ namespace EFCore.Bugs
             var serviceProvider = serviceCollection.BuildServiceProvider();
 
             CreateUsers(serviceProvider);
-            Issue(serviceProvider);
+	        Issue(serviceProvider);
             Resolve_1(serviceProvider);
             Resolve_2(serviceProvider);
+            Resolve_3(serviceProvider);
         }
 
         private static void Issue(ServiceProvider serviceProvider)
@@ -32,14 +33,35 @@ namespace EFCore.Bugs
                 {
                     Id = b.Id,
                     IsValue1 = b.Address.SomeEnum == SomeEnum.Value2 // if SomeEnum.Value2 == takeValue then throw Exception
-                }).ToList();
+                })
+                .ToList();
+        }
+
+        /// <summary>
+        /// Move 'Take' after 'Select'
+        /// </summary>
+        /// <param name="serviceProvider"></param>
+        private static void Resolve_1(ServiceProvider serviceProvider)
+        {
+	        using var db = serviceProvider.GetService<AppDbContext>();
+	        var takeValue = 2;
+	        var users = db.Users
+		        .Include(u => u.Address)
+		        .Select(b => new UserModel()
+		        {
+			        Id = b.Id,
+			        IsValue1 = b.Address.SomeEnum == SomeEnum.Value2
+		        })
+		        .Where(u => u.IsValue1)
+		        .Take(takeValue)
+		        .ToList();
         }
 
         /// <summary>
         /// Thanks to the "enumValue" variable, the SQL query is better, so this option is preferred if enum without 'FlagsAttribute'.
         /// </summary>
         /// <param name="serviceProvider"></param>
-        private static void Resolve_1(ServiceProvider serviceProvider)
+        private static void Resolve_2(ServiceProvider serviceProvider)
         {
             using var db = serviceProvider.GetService<AppDbContext>();
             var takeValue = 2;
@@ -51,14 +73,16 @@ namespace EFCore.Bugs
                 {
                     Id = b.Id,
                     IsValue1 = b.Address.SomeEnum == enumValue
-                }).ToList();
+                })
+                .Where(u=>u.IsValue1)
+                .ToList();
         }
 
         /// <summary>
         /// An alternative way to solve. But will be bitwise comparison instead direct comparison.
         /// </summary>
         /// <param name="serviceProvider"></param>
-        private static void Resolve_2(ServiceProvider serviceProvider)
+        private static void Resolve_3(ServiceProvider serviceProvider)
         {
             using var db = serviceProvider.GetService<AppDbContext>();
             var takeValue = 2;
